efe4d8599c3a183b21aca7126974eaf45bab30a7
Robert W <1263192+rbw@users.noreply.github.com>
2025-07-03 01:22:26 +0200

    Update redis queue repr

--- pyfiq/backends/redis.py
+++ pyfiq/backends/redis.py
@@ -6,6 +6,7 @@ from ._base import QueueBackend
 
 class RedisQueueBackend(QueueBackend):
     def __init__(self, redis_url="redis://localhost"):
+        self.redis_url = redis_url
         self.redis = redis.Redis.from_url(redis_url)
 
     def push(self, queue_name, task):
@@ -14,3 +15,6 @@ class RedisQueueBackend(QueueBackend):
     def pop(self, queue_name, timeout=1):
         item = self.redis.blpop(queue_name, timeout=timeout)
         return json.loads(item[1]) if item else None
+
+    def __repr__(self):
+        return f"RedisQueueBackend({self.redis_url})"
