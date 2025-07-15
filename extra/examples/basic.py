3de08d77af64772bebd31978cb35cf22adb1f6f6
Robert W <1263192+rbw@users.noreply.github.com>
2025-07-15 12:47:06 +0200

    Update example

--- extra/examples/basic.py
+++ extra/examples/basic.py
@@ -1,11 +1,13 @@
 import time
 import logging
+
 import requests
 
-from pyfiq import fifo, threaded_worker, RedisQueueBackend
+from pyfiq import fifo, ThreadedWorker
+
 
 logging.basicConfig(
-    level=logging.INFO,
+    level=logging.DEBUG,
     format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
     datefmt="%H:%M:%S",
 )
@@ -21,32 +23,35 @@ def handle_error(exc, task, _):
     log.exception(f"Task failed ({task})", exc_info=exc)
 
 
-@fifo(queue="google-https", on_success=handle_success, on_error=handle_error)
+@fifo(queue="google-http", on_success=handle_success, on_error=handle_error)
 def fetch_google_successfully():
     return requests.get("https://google.com")
 
 
-@fifo(queue="google-https", on_error=handle_error, max_retries=3)
+@fifo(queue="google-http", on_error=handle_error, max_retries=2, retry_wait=2)
 def fetch_google_fail(asdf):
     raise Exception("Simulated failure!")
 
 
-@fifo(queue="microsoft-https", on_success=handle_success)
+@fifo(queue="microsoft-http", on_success=handle_success)
 def fetch_microsoft():
     return requests.get("https://microsoft.com")
 
 
-def run_worker_loop():
-    threaded_worker(
-        backend=RedisQueueBackend("redis://localhost")
-    )
+def run_example():
+    worker = ThreadedWorker(redis_url="redis://localhost")
+    worker.start()
 
-    while True:
-        fetch_google_successfully()
-        fetch_google_fail()
-        fetch_microsoft()
+    try:
+        while True:
+            fetch_google_fail("duh!")
+            fetch_google_successfully()
+            fetch_microsoft()
+            time.sleep(10)
 
-        time.sleep(10)
+    except KeyboardInterrupt:
+        log.info("Shutting down...")
+        worker.stop()
 
 
-run_worker_loop()
+run_example()
